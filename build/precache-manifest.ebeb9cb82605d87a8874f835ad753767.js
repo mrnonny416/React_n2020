153ecfc41faf81ec761d172d1de557953d15680d
mrnonny <mrnonny@admin>
2020-09-16 12:29:47 +0000

    Edit Facebook from other and final finish

---
+++ build/precache-manifest.ebeb9cb82605d87a8874f835ad753767.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e0e3db2945282f3c61d565428bd482",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ad92508957c7e789a95",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "462dc449e0a7e122ef16",
+    "url": "/static/css/main.acce490e.chunk.css"
+  },
+  {
+    "revision": "0ad92508957c7e789a95",
+    "url": "/static/js/2.27b4ed2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.27b4ed2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "462dc449e0a7e122ef16",
+    "url": "/static/js/main.3701f116.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
